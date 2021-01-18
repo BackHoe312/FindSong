3e226fe2949191eeef1e34a3eee6e1974ecd4e0c
BackHoe312 <idb3131@naver.com>
2021-01-18 21:32:42 +0900

    페이징 기능 구현

--- index.php
+++ index.php
@@ -5,17 +5,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" type="text/css" href="/findsong/css/styles.css" />
+    <link rel="stylesheet" type="text/css" href="/findsong/styles.css" />
 </head>
 <body>
     <table class="list">
         <thead>
             <tr>
-                <th width="100">제목</th>
-                <th width="100">아티스트</th>
+                <th width="150">제목</th>
+                <th width="150">아티스트</th>
                 <th width="50">시간</th>
-                <th width="50">발매일</th>
-                <th width="100">앨범</th>
+                <th width="100">발매일</th>
+                <th width="150">앨범</th>
                 <th width="100">장르</th>
             </tr>
         </thead>
@@ -36,50 +36,61 @@
 
             $total_page = ceil($row_num / $list); // 페이징한 페이지 수 구하기
             if($block_end > $total_page) $block_end = $total_page; // 만약 블록의 마지막 번호가 페이지 수보다 많다면 마지막 번호는 페이지 수
+            $total_block = ceil($total_page / $block_ct);
             $start_num = ($page - 1) * $list;
 
             $sql2 = mq("select * from songDB order by idx limit $start_num, $list");
 
             while($Slist = $sql2->fetch_array()) { // fetch_array() = 한 행식 패치하여 배열로 저장
             // 테이블에서 id를 기준으로 내림차순해서 5개까지 표시
-                $sql = mq("select * from songDB order by idx desc limit 0,5");
-                while($Slist = $sql->fetch_array()) {
-                    // title 변수에 DB에서 가져온 title을 선택
-                    $title = $Slist["title"];
-                    if(strlen($title) > 30) {
-                        // title이 30을 넘으면 ...표시
-                        $title=str_replace($Slist["title"], mb_substr($Slist["title"], 0, 30, "utf-8")."...", $Slist["title"]);
-                    }
         ?>
         <tbody>
                 <tr>
                     <th width="150"><?php echo $Slist['title'] ?></th>
                     <th width="150"><?php echo $Slist['artist']?></th>
                     <th width="50"><?php echo $Slist['time'] ?></th>
-                    <th width="50"><?php echo $Slist['date'] ?></th>
-                    <th width="100"><?php echo $Slist['album'] ?></th>
+                    <th width="100"><?php echo $Slist['date'] ?></th>
+                    <th width="150"><?php echo $Slist['album'] ?></th>
                     <th width="100"><?php echo $Slist['genre'] ?></th>
                 </tr>
         </tbody>
-        <?php } ?>
+        <?php   } ?>
     </table>
     <div id="page_num">
             <ul>
                 <?php
-                    if($page <= 1) // 현재 페이지가 1이라면
-                        {echo "<li class='fo_re'>[<<]</li>";} // 링크 불가
-                    else {echo "<li><a href='?page=1'>[<<]</a></li>";} // 1번 페이지로 링크        
-                    for($i = $block_start; $i <= $block_end; $i++) {
-                        // 초기값을 블록의 시작번호를 조건으로 블록 시작 번호가 마지막 블록보다 작거나 같을 때까지 반복
-                        if($page == $i) {echo "<li class='fo_re'>[$i]</li>";} // 현재 페이지 표시
-                        else {echo "<li><a href='?page=$i'>[$i]</a></li>";}
+                    if($page <= 1)
+                    { //만약 page가 1보다 크거나 같다면
+                      echo "<li class='fo_re'>[<<]</li>"; //[<<]이라는 글자에 빨간색 표시 
+                    }else{
+                      echo "<li><a href='?page=1'>[<<]</a></li>"; //알니라면 [<<]글자에 1번페이지로 갈 수있게 링크
                     }
-                    if($page >= $total_page) {echo "<li class='fo_re'>[>>]</li>";} // 마지막 페이지 표시
-                    else {echo "<li><a href='?page=$total_page'>[>>]</a></li>";} // 마지막 페이지로 링크
+                    if(!($page <= 1))
+                    { //만약 page가 1보다 크거나 같다면 빈값
+                        $pre = $page-1; //pre변수에 page-1을 해준다 만약 현재 페이지가 3인데 이전버튼을 누르면 2번페이지로 갈 수 있게 함
+                        echo "<li><a href='?page=$pre'>[<]</a></li>"; //이전글자에 pre변수를 링크한다. 이러면 이전버튼을 누를때마다 현재 페이지에서 -1하게 된다.
+                    }
+                    for($i=$block_start; $i<=$block_end; $i++){ 
+                      //for문 반복문을 사용하여, 초기값을 블록의 시작번호를 조건으로 블록시작번호가 마지박블록보다 작거나 같을 때까지 $i를 반복시킨다
+                      if($page == $i){ //만약 page가 $i와 같다면 
+                        echo "<li class='fo_re'>[$i]</li>"; //현재 페이지에 해당하는 번호에 굵은 빨간색을 적용한다
+                      }else{
+                        echo "<li><a href='?page=$i'>[$i]</a></li>"; //아니라면 $i
+                      }
+                    }
+                    if($block_num >= $total_block){ //만약 현재 블록이 블록 총개수보다 크거나 같다면 빈 값
+                    }else{
+                      $next = $page + 1; //next변수에 page + 1을 해준다.
+                      echo "<li><a href='?page=$next'>[>]</a></li>"; //다음글자에 next변수를 링크한다. 현재 4페이지에 있다면 +1하여 5페이지로 이동하게 된다.
+                    }
+                    if($page >= $total_page){ //만약 page가 페이지수보다 크거나 같다면
+                      echo "<li class='fo_re'>[>>]</li>"; //마지막 글자에 긁은 빨간색을 적용한다.
+                    }else{
+                      echo "<li><a href='?page=$total_page'>[>>]</a></li>"; //아니라면 마지막글자에 total_page를 링크한다.
+                    }
 
                 ?>
             </ul>
         </div>
-    <?php } ?>
 </body>
 </html>
